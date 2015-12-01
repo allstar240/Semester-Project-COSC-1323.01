23e25c8bbc71d5668919113377147b9f2e399ca2
ajc732 <chen.alvinj@gmail.com>
2015-12-01 09:37:23 -0600

    Updated Password Entry
    Added password verification

--- project.py
+++ project.py
@@ -10,29 +10,75 @@ def displayMenu():
     return status
 
 def newUser():
-    tempLogin = '' #store username in temp until valid e-mail is entered then transfer to createLogin
-    createLogin = ''
-    while createLogin == '':
-        try:
-            tempLogin = input("Create login name.  It must be an .edu e-mail address: ")
-            if tempLogin in users: # check if login name exists
-                print ("\nLogin name already exist!\n")
-            elif '@' not in tempLogin:
-                print("Invalid entry.  Please enter a valid e-mail address.")
-            elif '.edu' not in tempLogin:
-                print("Invalid entry.  Please enter a valid .edu e-mail address.")
-            else:
-                createLogin = tempLogin
-        except:
-            print("Unexpected error.")
-    createPassw = input("Create password: ")
+    createLogin = verifyUsername()
+    createPassw = verifyPass()
     users[createLogin] = createPassw # add login and password
     print("\nUser created!\n")     
+    
+def verifyUsername():
+    tempLogin = '' #store username in temp until valid e-mail is entered then transfer to createLogin
+    createLogin = ''
+    retryUsername = 'y'
+    while createLogin == '':
+        while retryUsername == 'y':
+            try:
+                tempLogin = input("Create login name.  It must be an .edu e-mail address: ")
+                if tempLogin in users: # check if login name exists
+                    print ("\nLogin name already exist!\n")
+                    retryUsername = retryEntry()
+                elif '@' not in tempLogin:
+                    print("Invalid entry.  Please enter a valid e-mail address.")
+                    retryUsername = retryEntry()
+                elif '.edu' not in tempLogin:
+                    print("Invalid entry.  Please enter a valid .edu e-mail address.")
+                    retryUsername = retryEntry()
+                else:
+                    createLogin = tempLogin
+                    retryUsername = 'n'
+            except:
+                print("Unexpected error.")
+    return createLogin
+
+def retryEntry():
+    retryE = ''
+    while not (retryE == 'n' or retryE == 'y'):
+        retryE = input("Would you like to correct your entry? [y/n] ")
+        try:
+            if not (retryE == 'n' or retryE == 'y'):
+                print("That is not a valid option.")
+        except:
+            print("Unexpected error.")
+    return retryE
+
+def verifyPass():
+    tempPass = '' #store pass in temp until valid combination is entered then transfer to createPassw
+    createPassw = ''
+    retryPassw = 'y'
+    while createPassw == '':
+        while retryPassw == 'y':
+            try:
+                tempPass = input("Create password with 6-10 characters, at least one of which is a number: ")
+                print(len(tempPass))
+                print(hasNumbers(tempPass))
+                if len(tempPass) < 6 or len(tempPass) > 11: # check if login name exists
+                    print ("The password must be between 6 and 10 characters long.")
+                    retryPassw = retryEntry()
+                elif 5 < len(tempPass) < 11 and not hasNumbers(tempPass):
+                    print("Please include at least one number in your password.")
+                    retryPassw = retryEntry()
+                else:
+                    createPassw = tempPass
+                    retryPassw = 'n'
+            except:
+                print("Unexpected error.")
+    return createPassw    
+
+def hasNumbers(checkString):    
+    return any(char.isdigit() for char in checkString)
 
 def oldUser():
     login = input("Enter login name: ")
     passw = input("Enter password: ")
-
     if users[login] == passw: 
         print ("\nLogin successful!\n")
     else:
